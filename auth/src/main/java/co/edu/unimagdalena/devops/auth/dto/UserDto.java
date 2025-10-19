3181b3326268f805a319a1279f8a2a972e4b435c
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2025-10-18 21:15:38 -0500

    Entity and Dto of the User class

---
+++ auth/src/main/java/co/edu/unimagdalena/devops/auth/dto/UserDto.java
@@ -0,0 +1,28 @@
+package co.edu.unimagdalena.devops.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private UUID id;
+    private String name;
+    private String address;
+    private String email;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ auth/src/main/java/co/edu/unimagdalena/devops/auth/entity/User.java
@@ -0,0 +1,38 @@
+package co.edu.unimagdalena.devops.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    UUID id;
+    @Column
+    String name;
+    @Column
+    String address;
+    @Column
+    String email;
+    @Column
+    String password;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+}
