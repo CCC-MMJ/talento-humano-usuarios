a74a80049638f78f8fd740b6aa9208d8b89a8f1a
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2025-10-22 17:47:46 -0500

    New class controller for entity Profile was created

---
+++ auth/src/main/java/co/edu/unimagdalena/devops/auth/controller/ProfileController.java
@@ -0,0 +1,13 @@
+package co.edu.unimagdalena.devops.auth.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/profile")
+public class ProfileController {
+
+}
