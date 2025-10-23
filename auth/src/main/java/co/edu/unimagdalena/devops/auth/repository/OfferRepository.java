022e31b7a7e89f051b515ec425b67796cb48bb41
Jose Mendez <josealbertomendez13@gmail.com>
2025-10-22 20:02:46 -0500

    Creating repositorys (Company, Experiencie, Offer, Postulation)

---
+++ auth/src/main/java/co/edu/unimagdalena/devops/auth/repository/OfferRepository.java
@@ -0,0 +1,18 @@
+package co.edu.unimagdalena.devops.auth.repository;
+
+import co.edu.unimagdalena.devops.auth.entity.Offer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface OfferRepository extends JpaRepository<Offer, UUID> {
+    Optional<Offer> findById(UUID id);
+
+    Optional<Offer> findByTitleAndDescription(String title, String description);
+    List<Offer> findByTitleContaining(String title);
+    List<Offer> findByDescriptionContaining(String description);
+    List<Offer> findByTitleContainingAndDescriptionContaining(String title, String description);
+    Optional<Offer> findByTitle(UUID title, String description, UUID userId);
+}
---
+++ auth/src/main/java/co/edu/unimagdalena/devops/auth/repository/PostulationRepository.java
@@ -0,0 +1,4 @@
+package co.edu.unimagdalena.devops.auth.repository;
+
+public interface PostulationRepository {
+}
