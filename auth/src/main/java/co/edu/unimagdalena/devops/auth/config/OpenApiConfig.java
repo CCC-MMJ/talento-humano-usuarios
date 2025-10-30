709d9ac04796d8c3876126b7c5d390ea593c2ce5
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2025-10-30 16:53:15 -0500

    modifications in Swagger, new endpoint added in Profile Controller

--- auth/src/main/java/co/edu/unimagdalena/devops/auth/config/OpenApiConfig.java
+++ auth/src/main/java/co/edu/unimagdalena/devops/auth/config/OpenApiConfig.java
@@ -1,16 +1,39 @@
 package co.edu.unimagdalena.devops.auth.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
         info = @Info(
-                title = "Auth API",
-                version = "1.0",
-                description = "Documentación de la API de autenticación y perfiles"
-        )
+                title = "API AUTENTICACION",
+                description = "Documentación de la API de autenticación y perfiles",
+                termsOfService = "www.talentolocal.com/terminos_y_servicios",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "CCC-MMJ",
+                        url = "www.talentolocal.com/contact",
+                        email = "cacastrillon@unimagdalena.edu.co"
+                ),
+                license = @License(
+                        name = "Standard Software Use License for TalentoLocal",
+                        url = "www.talentolocal.com/contact"
+                )
+        ),
+        servers = {
+                @Server(
+                        description = "DEV SERVER",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "PROD SERVER",
+                        url = "jdbc:sqlserver://qa-devops-azure-sql-server.database.windows.net:1433"
+                )
+        }
 )
 public class OpenApiConfig {
 }
