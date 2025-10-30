57d122e2d25c92670cc703857715df2974e9086b
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2025-10-30 17:54:40 -0500

    Security Scheme created

--- auth/src/main/java/co/edu/unimagdalena/devops/auth/config/OpenApiConfig.java
+++ auth/src/main/java/co/edu/unimagdalena/devops/auth/config/OpenApiConfig.java
@@ -1,11 +1,16 @@
 package co.edu.unimagdalena.devops.auth.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 @OpenAPIDefinition(
@@ -33,7 +38,19 @@ import org.springframework.context.annotation.Configuration;
                         description = "PROD SERVER",
                         url = "jdbc:sqlserver://qa-devops-azure-sql-server.database.windows.net:1433"
                 )
-        }
+        },
+        security = @SecurityRequirement(
+                name = "Token de Seguridad"
+        )
+)
+@SecurityScheme(
+        name = "Token de Seguridad",
+        description = "Token de Acceso para mi API",
+        type = SecuritySchemeType.HTTP,
+        paramName = HttpHeaders.AUTHORIZATION,
+        in = SecuritySchemeIn.HEADER,
+        scheme = "bearer",
+        bearerFormat = "JWT"
 )
 public class OpenApiConfig {
 }
